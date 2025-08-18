1e42abdde17686b4d16e921791c0b8cba8d97e31
Ashfak Ahmath <ashfakahmath57@gmail.com>
2025-08-19 01:59:51 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const form = document.getElementById('contactForm');
-    form.addEventListener('submit', function(e) {
-      e.preventDefault();
-      alert('Thank you! Your message has been submitted.');
-      form.reset();
-    });
